c957d13aaa85fc8436261ba947ecec706b136934
FelipeFVieira <devfelipefranca@gmail.com>
2024-05-09 19:48:07 -0300

    feat booting project and create packages

---
+++ SpringBandAPI/src/main/java/com/restapi/SpringBandAPI/SpringBandApiApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.SpringBandAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBandApiApplication.class, args);
+	}
+
+}
