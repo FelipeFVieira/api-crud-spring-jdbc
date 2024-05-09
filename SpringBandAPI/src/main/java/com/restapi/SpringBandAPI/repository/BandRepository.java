3c552025ffe61f58c2d98765b1fb1c8a035929de
FelipeFVieira <devfelipefranca@gmail.com>
2024-05-09 20:12:47 -0300

    feat: add interface structure

--- SpringBandAPI/src/main/java/com/restapi/SpringBandAPI/repository/BandRepository.java
+++ SpringBandAPI/src/main/java/com/restapi/SpringBandAPI/repository/BandRepository.java
@@ -1,5 +1,45 @@
 package com.restapi.SpringBandAPI.repository;
 
-public class BandRepository {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.restapi.SpringBandAPI.model.Band;
+
+public class BandRepository implements MyCrud {
+	
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public List<Band> lista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Band buscaPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int altera(Band band, int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insere(Band band) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean exclui(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
